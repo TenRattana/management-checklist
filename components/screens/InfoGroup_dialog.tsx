96af704d3d96e2203a94fbfb2c819ad8095a5726
rattana.c <rattana.c@MIS-07>
2025-01-06 15:38:12 +0700

    Enhance InfoGroup dialog layout with theme integration, improved icon usage, and updated action button styling for better user experience

--- components/screens/InfoGroup_dialog.tsx
+++ components/screens/InfoGroup_dialog.tsx
@@ -1,9 +1,10 @@
 import { useRes } from "@/app/contexts/useRes";
+import { useTheme } from "@/app/contexts/useTheme";
 import useMasterdataStyles from "@/styles/common/masterdata";
 import React from "react";
 import { View, StyleSheet, TouchableOpacity } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Dialog, List, Text } from "react-native-paper";
+import { Dialog, Icon, IconButton, List, Text } from "react-native-paper";
 
 const InfoGroup_dialog = ({
     setDialogAdd,
@@ -16,11 +17,19 @@ const InfoGroup_dialog = ({
 }) => {
     const masterdataStyles = useMasterdataStyles();
     const { spacing } = useRes()
+    const { theme } = useTheme()
 
     return (
         <GestureHandlerRootView style={{ flexGrow: 1 }}>
+            <View style={{ justifyContent: "space-between", flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{ justifyContent: 'flex-start', flexDirection: 'row', alignItems: 'center' }}>
+                    <Icon source="information-outline" size={spacing.large} color={theme.colors.green} />
+                    <Text style={[masterdataStyles.title, masterdataStyles.textBold, { paddingLeft: 8 }]}>Create Group Check List Option & Option Detail</Text>
+                </View>
+                <IconButton icon="close" size={20} iconColor={theme.colors.black} onPress={() => setDialogAdd()} />
+            </View>
+
             <View style={styles.dialog}>
-                <Dialog.Title style={[masterdataStyles.text, masterdataStyles.textBold]}>{"Group Checklist Info"}</Dialog.Title>
                 <View style={{ marginHorizontal: 10 }}>
                     {option?.length > 0 ? (
                         option?.map((op, index) => (
@@ -36,11 +45,19 @@ const InfoGroup_dialog = ({
                         <Text style={masterdataStyles.text}>No options available</Text>
                     )}
                 </View>
-                <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-                    <TouchableOpacity onPress={() => setDialogAdd()} style={styles.actionButton}>
-                        <Text style={masterdataStyles.text}>Close</Text>
-                    </TouchableOpacity>
-                </View>
+            </View>
+
+            <View style={[masterdataStyles.containerAction, { paddingVertical: 10, justifyContent: "flex-end" }]}>
+                <TouchableOpacity
+                    onPress={() => setDialogAdd()}
+                    style={[masterdataStyles.backMain, styles.button, { marginLeft: 10, flexDirection: 'row' }]}
+                >
+                    <Icon source="close" size={spacing.large} color={theme.colors.fff} />
+
+                    <Text style={[masterdataStyles.text, masterdataStyles.textFFF, masterdataStyles.textBold, { paddingLeft: 15 }]}>
+                        Cancel
+                    </Text>
+                </TouchableOpacity>
             </View>
         </GestureHandlerRootView>
     );
@@ -51,14 +68,21 @@ export default InfoGroup_dialog;
 const styles = StyleSheet.create({
     dialog: {
         borderRadius: 10,
-        padding: 10,
+        paddingVertical: 10,
+    },
+    button: {
+        alignSelf: 'flex-end',
+        marginRight: 10,
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        alignItems: 'center',
+        borderRadius: 4,
     },
     title: {
         fontWeight: "bold",
         textAlign: "center",
         marginBottom: 10,
     },
-
     optionItem: {
         paddingVertical: 5,
     },
