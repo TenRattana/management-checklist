117825e212b59564b8782fe48fff6e3327062ae4
TenRattana <rattana.chomw@gmail.com>
2025-01-08 20:33:59 +0700

    Enhance createSubFormsAndFields function to initialize MCListID with default values for improved form handling

--- hooks/custom/useForm.tsx
+++ hooks/custom/useForm.tsx
@@ -71,7 +71,12 @@ const createSubFormsAndFields = async (
                         MaxLength: itemOption?.ImportantList[0]?.MaxLength,
                         Value
                     }]
-                    : [],
+                    :[{
+                        MCListID: itemOption.MCListID,
+                        Value: [],
+                        MinLength: undefined,
+                        MaxLength: undefined
+                    }],
                 Placeholder: itemOption.Placeholder,
                 Hint: itemOption.Hint,
                 DisplayOrder: itemOption.DisplayOrder,
